5df22cad0d36b870dc3277e5df98f7340451f62c
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2026-05-16 13:03:17 +0500

    use dj-database-url

--- hrmm_backend/config/settings/base.py
+++ hrmm_backend/config/settings/base.py
@@ -1,7 +1,8 @@
 import os
 from datetime import timedelta
 from pathlib import Path
-import dj_database_url
+from dj_database_url 
+
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
